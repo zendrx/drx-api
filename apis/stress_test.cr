32cd90b50a91d2237c237fb2d7533b66f607f79b
ZenDrx <ynwghosted@icloud.com>
2026-04-23 20:46:41 +0100

    Modify StressTest.run to accept request_count as String
    Change request_count type from Int32 to String and convert to Int32.

--- apis/stress_test.cr
+++ apis/stress_test.cr
@@ -2,7 +2,9 @@ require "crest"
 
 module Api
   class StressTest
-    def self.run(target_url : String, request_count : Int32 = 180)
+    def self.run(target_url : String, request_count : String)
+
+      request_count.to_i 
       # Hard cap for safety
       count = request_count > 200 ? 200 : request_count
       
