1e6c8933844986296b910c5ffa5c3edf64ed9740
ZenDrx <ynwghosted@icloud.com>
2026-04-23 17:08:58 +0100

    Fix formatting of QR code SVG generation

--- apis/qr.cr
+++ apis/qr.cr
@@ -5,7 +5,7 @@ require "qr-code"
      def self.run(text : String)
       begin  
        qr = QRCode.new(text)
-       svg_data = qr.as_svg(fill : "000", output_unit : "px")
+       svg_data = qr.as_svg(fill: "000", output_unit: "px")
        {
         "success" => "true",
         "data" => svg_data,
