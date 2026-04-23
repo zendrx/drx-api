e20141f6bc665b5c52b21988811921264e88c1dc
ZenDrx <ynwghosted@icloud.com>
2026-04-23 18:03:15 +0100

    Update news_cache.cr

--- apis/news_cache.cr
+++ apis/news_cache.cr
@@ -2,12 +2,11 @@ require "crest"
 require "json"
 
 module Api
-   type NewsItem = Hash(String, String | Nil)
-   @@cache = [] of NewsItem
+  alias NewsItem = Hash(String, String | Nil)
+
   class NewsCached
-    # This is our in-memory storage
-    
-    # Getter to let the Kemal route see the cache
+    @@cache = [] of NewsItem
+
     def self.cache
       @@cache
     end
@@ -17,8 +16,6 @@ module Api
       begin
         hn = fetch_hacker_news(10)
         dt = fetch_dev_to("programming", 10)
-        
-        # Overwrite the old cache with the new data
         @@cache = (hn + dt).shuffle
       rescue e : Exception
         puts "Cache refresh failed: #{e.message}"
@@ -31,21 +28,32 @@ module Api
       
       stories = [] of NewsItem
       ids.each do |id|
-      item_hash = { 
-       "title" => item["title"].as_s, 
-       "url" => item["url"]?.try(&.as_s), 
-       "source" => "HackerNews" 
-      } of String => String | Nil 
-    
-      stories << item_hash
+        begin
+          res = Crest.get("https://hacker-news.firebaseio.com/v0/item/#{id}.json")
+          item = JSON.parse(res.body)
+          
+          item_hash = { 
+            "title" => item["title"].as_s, 
+            "url" => item["url"]?.try(&.as_s), 
+            "source" => "HackerNews" 
+          } of String => String | Nil 
+          
+          stories << item_hash
+        rescue
+          next
+        end
+      end
+      stories
     end
-    stories
-   end
 
     private def self.fetch_dev_to(tag, limit)
       res = Crest.get("https://dev.to/api/articles?tag=#{tag}&per_page=#{limit}")
       JSON.parse(res.body).as_a.map do |a|
-        { "title" => a["title"].as_s, "url" => a["url"].as_s, "source" => "Dev.to" } of String => String | Nil
+        { 
+          "title" => a["title"].as_s, 
+          "url" => a["url"].as_s, 
+          "source" => "Dev.to" 
+        } of String => String | Nil
       end
     end
   end
